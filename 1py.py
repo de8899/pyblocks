88d5d9c6abaf154124aae227c5a8786eb384516b
unknown <derwin8899@gmail.com>
2020-12-07 19:17:14 -0800

    cleaned up text in input cmds on 1py.py

--- 1py.py
+++ 1py.py
@@ -2,15 +2,13 @@ import os
 import time
 import array
 
-def rearranger(**item):
-print(
 
 array1 = ["w42","dc41","w43"]
 
 for i in array1:
  print(i + " ------")
 
-print(array1[1] + " <- this is the 2nd member of the array")
+print(array1[1] + " <- This is the 2nd member of the array")
 
 val1 = input("Add a server name: ")
 print("You typed: " + val1)
